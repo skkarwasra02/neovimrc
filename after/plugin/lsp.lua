53f865a6484972f91d2c4d592cac83be0df069e1
= <=>
2024-09-08 09:10:21 +0000

    First commit

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,78 @@
+local lsp_zero = require('lsp-zero')
+
+-- lsp_attach is where you enable features that only work
+-- if there is a language server active in the file
+local lsp_attach = function(client, bufnr)
+	local opts = {buffer = bufnr}
+
+	vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+	vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+	vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+	vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+	vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+	vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+	vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+	vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+	vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+	vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+end
+
+lsp_zero.extend_lspconfig({
+	sign_text = true,
+	lsp_attach = lsp_attach,
+	capabilities = require('cmp_nvim_lsp').default_capabilities(),
+})
+
+local lspconfig = require('lspconfig')
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  -- Replace the language servers listed here
+  -- with the ones you want to install
+  -- ensure_installed = {'tsserver', 'rust_analyzer'},
+  ensure_installed = {
+    'tsserver',
+    'eslint',
+    'html',
+    'cssls'
+  },
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+    ['tsserver'] = function()
+      lspconfig.tsserver.setup({
+        capabilities = lsp_capabilities,
+        settings = {
+          completions = {
+            completeFunctionCalls = true
+          }
+        }
+      })
+    end
+  }
+})
+
+local cmp = require('cmp')
+local cmp_format = require('lsp-zero').cmp_format({details = true})
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+    {name = 'buffer'},
+    {name = 'codeium'},
+  },
+  --- (Optional) Show source name in completion menu
+  formatting = cmp_format,
+  mapping = cmp.mapping.preset.insert({
+    -- confirm completion
+    ['<C-y>'] = cmp.mapping.confirm({select = true}),
+
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- scroll up and down the documentation window
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),   
+  }),
+})
