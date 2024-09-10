5f339a357eb4aeda05fe6870780eda3e68fc8d3a
= <=>
2024-09-10 07:07:44 +0000

    tokyonight installed

--- lua/sk/packer.lua
+++ lua/sk/packer.lua
@@ -4,40 +4,47 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  use({
-	'rose-pine/neovim',
-	as = 'rose-pine',
-	config = function()
-		vim.cmd('colorscheme rose-pine')
-	end
-  })
+    -- use({
+    --     'rose-pine/neovim',
+    --     as = 'rose-pine',
+    --     config = function()
+    --         vim.cmd('colorscheme rose-pine')
+    --     end
+    -- })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
 
-  use({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
-  use({'neovim/nvim-lspconfig'})
-  use({'williamboman/mason.nvim'})
-  use({'williamboman/mason-lspconfig.nvim'})
-  use({'hrsh7th/nvim-cmp'})
-  use({'hrsh7th/cmp-buffer'})
-  use({'hrsh7th/cmp-path'})
-  use({'saadparwaiz1/cmp_luasnip'})
-  use({'hrsh7th/cmp-nvim-lsp'})
-  use({'hrsh7th/cmp-nvim-lua'})
-  use({'L3MON4D3/LuaSnip'})
-  use({'rafamadriz/friendly-snippets'})
+    use({ 'VonHeikemen/lsp-zero.nvim', branch = 'v4.x' })
+    use({ 'neovim/nvim-lspconfig' })
+    use({ 'williamboman/mason.nvim' })
+    use({ 'williamboman/mason-lspconfig.nvim' })
+    use({ 'hrsh7th/nvim-cmp' })
+    use({ 'hrsh7th/cmp-buffer' })
+    use({ 'hrsh7th/cmp-path' })
+    use({ 'saadparwaiz1/cmp_luasnip' })
+    use({ 'hrsh7th/cmp-nvim-lsp' })
+    use({ 'hrsh7th/cmp-nvim-lua' })
+    use({ 'L3MON4D3/LuaSnip' })
+    use({ 'rafamadriz/friendly-snippets' })
 
-  use({'Exafunction/codeium.vim'})
+    use({ 'Exafunction/codeium.vim' })
+
+    use({
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    })
 end)
