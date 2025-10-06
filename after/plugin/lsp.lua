1105ebe2ede7c422ccc8f0519ea1d78b8a095f5b
= <=>
2025-10-06 09:41:35 +0000

    null_ls removed

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -30,10 +30,14 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
     -- Replace the language servers listed here
     -- with the ones you want to install
-    -- ensure_installed = {'tsserver', 'rust_analyzer'},
     ensure_installed = {
+        'clangd',
+        'rust_analyzer',
+        'pylsp',
+        'lua_ls',
         'ts_ls',
         'eslint',
+        'jsonls',
         'html',
         'cssls'
     },
@@ -132,41 +136,4 @@ prettier.setup({
         "yaml",
         "lua"
     },
-})
-
--- local null_ls = require("null-ls")
-
-local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
-local event = "BufWritePre" -- or "BufWritePost"
-local async = event == "BufWritePost"
-
-null_ls.setup({
-    sources = {
-        -- null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.formatting.prettierd,
-    },
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.keymap.set("n", "<Leader>f", function()
-                vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-            end, { buffer = bufnr, desc = "[lsp] format" })
-
-            -- format on save
-            vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-            vim.api.nvim_create_autocmd(event, {
-                buffer = bufnr,
-                group = group,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr, async = async })
-                end,
-                desc = "[lsp] format on save",
-            })
-        end
-
-        if client.supports_method("textDocument/rangeFormatting") then
-            vim.keymap.set("x", "<Leader>f", function()
-                vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-            end, { buffer = bufnr, desc = "[lsp] format" })
-        end
-    end,
 })
