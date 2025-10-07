b161c62b3d93371751d9fa810022b092eff2bd5c
= <=>
2025-10-07 17:43:01 +0000

    Plugins and keymaps updated

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,10 @@
+return {
+  -- Disable lazyvim plugins
+  { "akinsho/bufferline.nvim", enabled = false },
+
+  -- Add additional plugins
+  {
+    "theprimeagen/harpoon",
+    enabled = true,
+  },
+}
